444e7c642bb88c1268ee930b3ee6c91c6a4cc662
rihitosan <76610702+rihitosan@users.noreply.github.com>
2021-03-13 15:25:25 +0900

    Update menu.js

--- menu.js
+++ menu.js
@@ -34,7 +34,7 @@ if(document.getElementById("d_menu") != null){
   document.getElementById("d_menu").innerHTML=d_menu_html
 }
 
-document.bgColor = "#cccccc";
+document.bgColor = "#fdfdfd";
 
 
 //下のメニュー
