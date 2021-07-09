5cd744f4a6924b8f4eea3fa6e0628dcc202ea82a
rihitosan <76610702+rihitosan@users.noreply.github.com>
2021-07-09 19:16:14 +0900

    Add files via upload

--- menu.js
+++ menu.js
@@ -2,9 +2,10 @@ var url = "https://rihitosan.com/"
 
 logo_img_html = ''
 + '<a href="'+url+'" class="menu_link" style="float:left;">Home</a>'
-+ '<a href="'+url+'/game" class="menu_link">Game</a>'
-+ '<a href="qiita.com/rihitosan" class="menu_link">Qiita</a>'
-+ '<a href="github.com/rihitosan" class="menu_link">Github</a>'
++ '<a href="'+url+'game/" class="menu_link">Game</a>'
++'<a href="'+url+'blog/" class="menu_link">Blog</a>'
++ '<a href="http://qiita.com/rihitosan" class="menu_link">Qiita</a>'
++ '<a href="http://github.com/rihitosan" class="menu_link">Github</a>'
 + '<a href="https://forms.gle/13LRRauS1vfLeGXq6" class="menu_link">contact</a>'
 
 if(document.getElementById("logo_img").innerHTML != null){
